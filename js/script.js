b5b5e7cf7ab843277cf4801045b38b0134b2a701
mirco verderosa <verderosamircowork@gmail.com>
2021-01-27 15:09:18 +0100

    Aggiunta funzionalità  Prev e Next allo slider

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+var app = new Vue({
+  el: '#container',
+  data:{
+
+    immagini:[
+
+      "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+
+      "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
+
+    ],
+
+    idxImages: 0,
+
+  },
+
+  methods:{
+   nextClick: function(){
+     return this.idxImages += 1;
+   },
+
+   prevClick : function(){
+     return this.idxImages -= 1;
+   }
+
+  }
+});
+
+
+
+
+Vue.config.devtools = true;
